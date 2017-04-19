501bd534d9021f7bb17e743565d146af6023ce49
Kristaps Karlsons <kristaps.karlsons@gmail.com>
2017-04-19 23:45:58 +0300

    Minor cleanup, BSD auth removal

--- sflock.c
+++ sflock.c
@@ -14,18 +14,13 @@
 #include <sys/types.h>
 #include <sys/ioctl.h>
 #include <linux/vt.h>
-#include <fcntl.h> 
-#include <errno.h> 
+#include <fcntl.h>
+#include <errno.h>
 #include <termios.h>
 #include <X11/keysym.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/extensions/dpms.h>
-
-#if HAVE_BSD_AUTH
-#include <login_cap.h>
-#include <bsd_auth.h>
-#endif
 
 static void
 die(const char *errstr, ...) {
@@ -37,17 +32,16 @@ die(const char *errstr, ...) {
     exit(EXIT_FAILURE);
 }
 
-#ifndef HAVE_BSD_AUTH
 static const char *
 get_password() { /* only run as root */
     const char *rval;
     struct passwd *pw;
 
-    if(geteuid() != 0)
+    if (geteuid() != 0)
         die("sflock: cannot retrieve password entry (make sure to suid sflock)\n");
     pw = getpwuid(getuid());
     endpwent();
-    rval =  pw->pw_passwd;
+    rval = pw->pw_passwd;
 
 #if HAVE_SHADOW_H
     {
@@ -63,17 +57,14 @@ get_password() { /* only run as root */
         die("sflock: cannot drop privileges\n");
     return rval;
 }
-#endif
 
 int
 main(int argc, char **argv) {
     char curs[] = {0, 0, 0, 0, 0, 0, 0, 0};
-    char buf[32], passwd[256], passdisp[256];
+    char buf[32], passwd[256];
     int num, screen, width, height, update, sleepmode, term, pid;
 
-#ifndef HAVE_BSD_AUTH
     const char *pws;
-#endif
     unsigned int len;
     Bool running = True;
     Cursor invisible;
@@ -84,49 +75,8 @@ main(int argc, char **argv) {
     XColor black, red, dummy;
     XEvent ev;
     XSetWindowAttributes wa;
-    XFontStruct* font;
-    GC gc; 
+    GC gc;
     XGCValues values;
-
-    // defaults
-    char* passchar = "*";
-    char* fontname = "-*-dejavu sans-bold-r-*-*-*-90-100-100-*-*-iso8859-1";
-    char* username = ""; 
-    int showline = 1;
-    int xshift = 0;
-
-    for (int i = 0; i < argc; i++) {
-        if (!strcmp(argv[i], "-c")) {
-            if (i + 1 < argc) 
-                passchar = argv[i + 1];
-            else
-                die("error: no password character given.\n");
-        } else
-            if (!strcmp(argv[i], "-f")) {
-                if (i + 1 < argc) 
-                    fontname = argv[i + 1];
-                else
-                    die("error: font not specified.\n");
-            }
-            else
-                if (!strcmp(argv[i], "-v")) 
-                    die("sflock-"VERSION", © 2015 Ben Ruijl\n");
-                else 
-                    if (!strcmp(argv[i], "-h")) 
-                        showline = 0;
-                    else 
-                        if (!strcmp(argv[i], "-xshift"))
-                            xshift = atoi(argv[i + 1]);
-                        else
-                            if (!strcmp(argv[i], "?"))
-                                die("usage: sflock [-v] [-c passchars] [-f fontname] [-xshift horizontal shift]\n");
-    }
-
-    // fill with password characters
-    for (int i = 0; i < sizeof passdisp; i+= strlen(passchar)) 
-        for (int j = 0; j < strlen(passchar) && i + j < sizeof passdisp; j++)
-            passdisp[i + j] = passchar[j];
-
 
     /* disable tty switching */
     if ((term = open("/dev/console", O_RDWR)) == -1) {
@@ -134,24 +84,19 @@ main(int argc, char **argv) {
     }
 
     if ((ioctl(term, VT_LOCKSWITCH)) == -1) {
-        perror("error locking console"); 
+        perror("error locking console");
     }
 
     /* deamonize */
     pid = fork();
-    if (pid < 0) 
+    if (pid < 0)
         die("Could not fork sflock.");
-    if (pid > 0) 
+    if (pid > 0)
         exit(0); // exit parent 
 
-#ifndef HAVE_BSD_AUTH
     pws = get_password();
-    username = getpwuid(geteuid())->pw_name;
-#else
-    username = getlogin();
-#endif
 
-    if(!(dpy = XOpenDisplay(0)))
+    if (!(dpy = XOpenDisplay(0)))
         die("sflock: cannot open dpy\n");
 
     screen = DefaultScreen(dpy);
@@ -161,37 +106,38 @@ main(int argc, char **argv) {
 
     wa.override_redirect = 1;
     wa.background_pixel = XBlackPixel(dpy, screen);
-    w = XCreateWindow(dpy, root, 0, 0, width, height,
-            0, DefaultDepth(dpy, screen), CopyFromParent,
-            DefaultVisual(dpy, screen), CWOverrideRedirect | CWBackPixel, &wa);
+    w = XCreateWindow(
+        dpy, root, 0, 0, width, height,
+        0, DefaultDepth(dpy, screen), CopyFromParent,
+        DefaultVisual(dpy, screen),
+        CWOverrideRedirect | CWBackPixel, &wa);
 
-    XAllocNamedColor(dpy, DefaultColormap(dpy, screen), "orange red", &red, &dummy);
-    XAllocNamedColor(dpy, DefaultColormap(dpy, screen), "black", &black, &dummy);
+    XAllocNamedColor(
+        dpy, DefaultColormap(dpy, screen), "orange red", &red, &dummy);
+    XAllocNamedColor(
+        dpy, DefaultColormap(dpy, screen), "black", &black, &dummy);
     pmap = XCreateBitmapFromData(dpy, w, curs, 8, 8);
     invisible = XCreatePixmapCursor(dpy, pmap, pmap, &black, &black, 0, 0);
     XDefineCursor(dpy, w, invisible);
     XMapRaised(dpy, w);
 
-    font = XLoadQueryFont(dpy, fontname);
-
-    if (font == 0) {
-        die("error: could not find font. Try using a full description.\n");
-    }
-
-    gc = XCreateGC(dpy, w, (unsigned long)0, &values);
-    XSetFont(dpy, gc, font->fid);
+    gc = XCreateGC(dpy, w, 0, &values);
     XSetForeground(dpy, gc, XWhitePixel(dpy, screen));
 
-    for(len = 1000; len; len--) {
-        if(XGrabPointer(dpy, root, False, ButtonPressMask | ButtonReleaseMask | PointerMotionMask,
-                    GrabModeAsync, GrabModeAsync, None, invisible, CurrentTime) == GrabSuccess)
+    for (len = 1000; len; len--) {
+        if (XGrabPointer(
+            dpy, root, False,
+            ButtonPressMask | ButtonReleaseMask | PointerMotionMask,
+            GrabModeAsync, GrabModeAsync, None, invisible,
+            CurrentTime) == GrabSuccess)
             break;
         usleep(1000);
     }
-    if((running = running && (len > 0))) {
-        for(len = 1000; len; len--) {
-            if(XGrabKeyboard(dpy, root, True, GrabModeAsync, GrabModeAsync, CurrentTime)
-                    == GrabSuccess)
+    if ((running = running && (len > 0))) {
+        for (len = 1000; len; len--) {
+            if (XGrabKeyboard(
+                dpy, root, True, GrabModeAsync, GrabModeAsync, CurrentTime)
+                == GrabSuccess)
                 break;
             usleep(1000);
         }
@@ -204,7 +150,7 @@ main(int argc, char **argv) {
     sleepmode = False;
 
     /* main event loop */
-    while(running && !XNextEvent(dpy, &ev)) {
+    while (running && !XNextEvent(dpy, &ev)) {
         if (sleepmode) {
             DPMSEnable(dpy);
             DPMSForceLevel(dpy, DPMSModeOff);
@@ -212,15 +158,7 @@ main(int argc, char **argv) {
         }
 
         if (update) {
-            int x, y, dir, ascent, descent;
-            XCharStruct overall;
-
             XClearWindow(dpy, w);
-            XTextExtents (font, passdisp, len, &dir, &ascent, &descent, &overall);
-            x = (width - overall.width) / 2;
-            y = (height + ascent - descent) / 2;
-
-            XDrawString(dpy,w,gc, x + xshift, y, passdisp, len);
             update = False;
         }
 
@@ -228,30 +166,26 @@ main(int argc, char **argv) {
             sleepmode = False;
         }
 
-        if(ev.type == KeyPress) {
+        if (ev.type == KeyPress) {
             sleepmode = False;
 
             buf[0] = 0;
             num = XLookupString(&ev.xkey, buf, sizeof buf, &ksym, 0);
-            if(IsKeypadKey(ksym)) {
-                if(ksym == XK_KP_Enter)
+            if (IsKeypadKey(ksym)) {
+                if (ksym == XK_KP_Enter)
                     ksym = XK_Return;
-                else if(ksym >= XK_KP_0 && ksym <= XK_KP_9)
+                else if (ksym >= XK_KP_0 && ksym <= XK_KP_9)
                     ksym = (ksym - XK_KP_0) + XK_0;
             }
-            if(IsFunctionKey(ksym) || IsKeypadKey(ksym)
-                    || IsMiscFunctionKey(ksym) || IsPFKey(ksym)
-                    || IsPrivateKeypadKey(ksym))
+            if (IsFunctionKey(ksym) || IsKeypadKey(ksym)
+                || IsMiscFunctionKey(ksym) || IsPFKey(ksym)
+                || IsPrivateKeypadKey(ksym))
                 continue;
 
-            switch(ksym) {
+            switch (ksym) {
                 case XK_Return:
                     passwd[len] = 0;
-#ifdef HAVE_BSD_AUTH
-                    running = !auth_userokay(getlogin(), NULL, "auth-xlock", passwd);
-#else
                     running = strcmp(crypt(passwd, pws), pws);
-#endif
                     if (running != 0)
                         // change background on wrong password
                         XSetWindowBackground(dpy, w, red.pixel);
@@ -266,11 +200,12 @@ main(int argc, char **argv) {
 
                     break;
                 case XK_BackSpace:
-                    if(len)
+                    if (len)
                         --len;
                     break;
                 default:
-                    if(num && !iscntrl((int) buf[0]) && (len + num < sizeof passwd)) { 
+                    if (num && !iscntrl((int) buf[0]) &&
+                        (len + num < sizeof passwd)) {
                         memcpy(passwd + len, buf, num);
                         len += num;
                     }
@@ -285,16 +220,14 @@ main(int argc, char **argv) {
     /* free and unlock */
     setreuid(geteuid(), 0);
     if ((ioctl(term, VT_UNLOCKSWITCH)) == -1) {
-        perror("error unlocking console"); 
+        perror("error unlocking console");
     }
 
     close(term);
     setuid(getuid()); // drop rights permanently
 
-
     XUngrabPointer(dpy, CurrentTime);
     XFreePixmap(dpy, pmap);
-    XFreeFont(dpy, font);
     XFreeGC(dpy, gc);
     XDestroyWindow(dpy, w);
     XCloseDisplay(dpy);
